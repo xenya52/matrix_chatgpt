b5ac1ae453bc1381ead9f8b5b3a0f691a51ec6e9
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-12-15 19:28:43 +0100

    fixed setup :)

--- target/debug/deps/matrix_chatgpt-da39d79326608d49.d
+++
@@ -1,5 +0,0 @@
-/home/xenya/Documents/Development/RustStuff/matrix_chatgpt/target/debug/deps/matrix_chatgpt-da39d79326608d49: src/main.rs
-
-/home/xenya/Documents/Development/RustStuff/matrix_chatgpt/target/debug/deps/matrix_chatgpt-da39d79326608d49.d: src/main.rs
-
-src/main.rs:
--- target/debug/matrix_chatgpt.d
+++
@@ -1,1 +0,0 @@
-/home/xenya/Documents/Development/RustStuff/matrix_chatgpt/target/debug/matrix_chatgpt: /home/xenya/Documents/Development/RustStuff/matrix_chatgpt/src/main.rs
