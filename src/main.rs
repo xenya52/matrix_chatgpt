90cbf0f1d40c7431ca8795057c07ebbdefe35283
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-12-28 17:32:58 +0100

    add openai_api_rs

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,31 @@ use matrix_sdk::{
 };
 use std::env;
 use dotenv::dotenv;
+use openai_api_rs::v1::api::Client as OtherClient;
+use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use openai_api_rs::v1::common::GPT4;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
-    // Access environment variables
+
+    // Access environment variables*
     let matrix_password = env::var("MATRIX_PASSWORD")?;
+    let gpt_client = OtherClient::new(env::var("OPENAI_API_KEY").unwrap().to_string());
 
     let alice = user_id!("@ll52_bot:matrix.org");
-    let client = Client::builder().user_id(alice.clone()).build().await?;
+    let matrix_client = Client::builder().user_id(alice.clone()).build().await?;
 
     // First we need to log in.
-    client.login_username(alice, &matrix_password).send().await?;
+    matrix_client.login_username(alice, &matrix_password).send().await?;
 
-    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+    matrix_client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
         println!("Received a message {:?}", ev);
     });
-
+    
     // Syncing is important to synchronize the client state with the server.
     // This method will never return.
-    client.sync(SyncSettings::default()).await;
+    matrix_client.sync(SyncSettings::default()).await;
 
     Ok(())
 }
