dea3580a6cee0dec5b53856c8b47bc4c42be8e99
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-12-29 10:06:40 +0100

    add matrix_event1 and gpt_event1

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,31 @@ use dotenv::dotenv;
 use openai_api_rs::v1::api::Client as OtherClient;
 use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
 use openai_api_rs::v1::common::GPT4;
+use anyhow::Result;
+
+async fn matrix_event1(client: &Client) {
+    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+        //println!("Received a message {:?}", ev);
+        println!("add_event_handler worked!");
+    });
+}
+
+async fn gpt_event1(client: &OtherClient) -> anyhow::Result<()> {
+
+    let req = ChatCompletionRequest::new(
+        GPT4.to_string(),
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: String::from("What is Bitcoin?"),
+            name: None,
+            function_call: None,
+        }],
+    );
+    let result = client.chat_completion(req)?;
+    println!("{:?}", result.choices[0].message.content);
+    println!("Gpt worked!");
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -22,13 +47,13 @@ async fn main() -> anyhow::Result<()> {
     // First we need to log in.
     matrix_client.login_username(alice, &matrix_password).send().await?;
 
-    matrix_client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
-        println!("Received a message {:?}", ev);
-    });
-    
+    matrix_event1(&matrix_client);
+
     // Syncing is important to synchronize the client state with the server.
     // This method will never return.
     matrix_client.sync(SyncSettings::default()).await;
 
+    gpt_event1(&gpt_client);
+    
     Ok(())
 }
